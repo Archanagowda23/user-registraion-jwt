13fd265e4cde2c2535c951bbb7d1a232a662d974
Archanagowda23 <archanar249@gmail.com>
2022-06-13 14:38:55 +0530

    Added user-registraion program to generate token.

---
+++ src/main/java/com/bridgelabz/JwtUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUserRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtUserRegistrationApplication.class, args);
+    }
+
+}
